8c01387a95c3431315b7468c9b7e7943208fc394
S0l4RIS <silverphantom15@gmail.com>
2025-01-25 17:05:17 +1100

    Updated for Netlify

---
+++ netlify/functions/index.js
@@ -0,0 +1,30 @@
+const fetch = require('node-fetch');
+
+exports.handler = async function (event, context) {
+  const { url } = event.queryStringParameters;
+  if (!url) {
+    return {
+      statusCode: 400,
+      body: 'Missing "url" query parameter.',
+    };
+  }
+
+  try {
+    const response = await fetch(url);
+    const html = await response.text();
+    return {
+      statusCode: 200,
+      headers: {
+        'Cache-Control': 's-maxage=600, stale-while-revalidate=59',
+        'Content-Type': 'text/html; charset=utf-8',
+      },
+      body: html,
+    };
+  } catch (error) {
+    console.error('Error fetching the webpage:', error.message);
+    return {
+      statusCode: 500,
+      body: 'Error fetching the webpage.',
+    };
+  }
+};
